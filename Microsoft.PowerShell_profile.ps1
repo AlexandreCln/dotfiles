5e87f335e36879a43f78005b21265eed7a4d4a87
Alexandre COULON <alexandre.coulon@sii.fr>
2022-03-08 18:53:04 +0100

    Powershell profile file.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+Clear-Host # clear terminal (avoid a current display bug)
+
+Set-PoshPrompt -Theme powerlevel10k_rainbow
+#Set-PoshPrompt hotstick.minimal
+#Set-PoshPrompt marcduiker
+#Set-PoshPrompt -Theme patriksvensson
+#Set-PoshPrompt -Theme rudolfs-dark
+#Set-PoshPrompt -Theme mojada
+
+# Aliases
+New-Alias edit "C:\Program Files (x86)\Notepad++\notepad++.exe"
